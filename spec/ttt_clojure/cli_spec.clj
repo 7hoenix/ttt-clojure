85479ba786010a6e842e91f0a9e9d1a6b3694175
Justin Holzmann <jholzmann@8thlight.com>
2016-04-29 11:32:27 -0500

    Adds first failing test (requiring the file itself).

---
+++ spec/ttt_clojure/cli_spec.clj
@@ -0,0 +1,16 @@
+(ns ttt-clojure.cli-spec
+  (:require [speclj.core :refer :all]
+            [ttt-clojure.cli :as cli]))
+
+(describe "cli")
+
+; UI, start a game, create a board. Human v. Human.
+; Testing UI (what comes in and how do I test it).
+
+;  (it "checks if game is over"
+
+; UI interface
+;  (it "prompts a user to play a game"
+;  (it "gets input from user"
+;  (it "prints board"
+;  (it "asks for next move"
