93c934d74f91f6ddcac0a22fb8ebfcb759c3d4b9
Justin Holzmann <jholzmann@8thlight.com>
2016-04-29 08:53:12 -0500

    Changes to vector instead of list as the order of the sequence shouldn't matter.

--- src/ttt_clojure/board.clj
+++ src/ttt_clojure/board.clj
@@ -2,7 +2,7 @@
 
 (def available-mark " ")
 (def x-mark "X")
-(def winning-seqs '(0 1 2))
+(def winning-seqs [0 1 2])
 
 (defn- filter-indexed [f coll]
   (filter f (map-indexed (fn [a b] [a b]) coll)))
