5d89834ac0b8b615f194969bb69c386e7b9b7f1a
Justin Holzmann <jholzmann@8thlight.com>
2016-05-02 08:57:56 -0500

    Adds test for get-input.

--- spec/ttt_clojure/cli_spec.clj
+++ spec/ttt_clojure/cli_spec.clj
@@ -2,15 +2,16 @@
   (:require [speclj.core :refer :all]
             [ttt-clojure.cli :as cli]))
 
-(describe "cli")
+(describe "command line interface or cli"
+  (it "gets input from user"
+        (should=
+          "cake"
+          (with-in-str "cake" (cli/get-input "pie or cake?")))))
+
 
 ; UI, start a game, create a board. Human v. Human.
 ; Testing UI (what comes in and how do I test it).
 
-;  (it "checks if game is over"
-
 ; UI interface
-;  (it "prompts a user to play a game"
-;  (it "gets input from user"
 ;  (it "prints board"
 ;  (it "asks for next move"
---
+++ src/ttt_clojure/cli.clj
@@ -0,0 +1,1 @@
+(ns ttt-clojure.cli)
