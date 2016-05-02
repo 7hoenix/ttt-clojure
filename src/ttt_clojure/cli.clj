1205d8e80b27d4d6989370f261b79ef6f099b4b1
Justin Holzmann <jholzmann@8thlight.com>
2016-05-02 11:55:35 -0500

    Adds yes/no.

--- src/ttt_clojure/cli.clj
+++ src/ttt_clojure/cli.clj
@@ -10,7 +10,7 @@
   (let [message "Would you like to start a new game? (enter y or n)"
         response (get-input message)]
     (cond
-      (= response "y") true
-      (= response "n") false
+      (or (= response "y") (= response "yes")) true
+      (or (= response "n") (= response "no")) false
       :else (new-game))))
 
