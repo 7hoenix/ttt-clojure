1168c6f0f2a1e7df3e514197c1735011d1166ab9
Justin Holzmann <jholzmann@8thlight.com>
2016-05-02 09:15:48 -0500

    Basic test for a user starting a new game.

--- src/ttt_clojure/cli.clj
+++ src/ttt_clojure/cli.clj
@@ -3,3 +3,10 @@
 (defn get-input [question]
   (println question)
   (read-line))
+
+(defn new-game []
+  (let [message "Would you like to start a new game? (enter y or n)"
+        response (get-input message)]
+    (if (= response "y")
+      true
+      false)))
