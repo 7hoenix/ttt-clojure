4dfbb427fde932757691a0db0ec8abd5023f38a4
Justin Holzmann <jholzmann@8thlight.com>
2016-05-05 09:57:12 -0500

    ga .! Starting over game loop from scratch. I think this will be better.

--- spec/ttt_clojure/game_spec.clj
+++ spec/ttt_clojure/game_spec.clj
@@ -7,41 +7,91 @@
 (describe "game loop"
   (with-stubs)
 
-  (it "ends the game if there is a winner"
-      (with-redefs [cli/report-winner (fn [_] "X wins")
-                    cli/prompt-move (fn [_ _]
-                                      {:location 0
-                                       :player "X"})]
-        (should=
-          "X wins"
-          (-> [" " " " " "
-               " " "X" " "
-               " " " " "X"]
-              (game/start "X")))))
+  (it "ends the game if it has a winner"
+      (with-redefs [cli/report-winner (fn [_] "X wins")]
+        (let [game (game/new-game "X" "O")]
+      (should= 
+        "X wins"
+        (game/check-for-winner game current-player))))))
 
-  (it "ends the game if it is tied"
-      (with-redefs [cli/report-tie (fn [_] "Cats game")
-                    cli/prompt-move (fn [_ _]
-                                      {:location 0
-                                       :player "O"})]
-        (should=
-          "Cats game"
-          (-> [" " "X" "O"
-               "O" "O" "X"
-               "X" "O" "X"]
-              (game/start "O")))))
 
-  (it "prompts the next player for their next move"
-      (let [game (game/new-game "X" "O")]
-      (should=
-        "X"
-        (game/take-next-turn game "O"))))
 
-  (it "prompts the next player for their next move"
-      (let [game (game/new-game "X" "O")]
-      (should=
-        "O"
-        (game/take-next-turn game "X")))))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  ; (it "ends the game if there is a winner"
+  ;     (with-redefs [cli/report-winner (fn [_] "X wins")
+  ;                   cli/prompt-move (fn [_ _]
+  ;                                     {:location 0
+  ;                                      :player "X"})]
+  ;       (should=
+  ;         "X wins"
+  ;         (-> [" " " " " "
+  ;              " " "X" " "
+  ;              " " " " "X"]
+  ;             (game/start "X")))))
+
+  ; (it "ends the game if it is tied"
+  ;     (with-redefs [cli/report-tie (fn [_] "Cats game")
+  ;                   cli/prompt-move (fn [_ _]
+  ;                                     {:location 0
+  ;                                      :player "O"})]
+  ;       (should=
+  ;         "Cats game"
+  ;         (-> [" " "X" "O"
+  ;              "O" "O" "X"
+  ;              "X" "O" "X"]
+  ;             (game/start "O")))))
+
+  ; (it "prompts the next player for their next move"
+  ;     (let [game (game/new-game "X" "O")]
+  ;     (should=
+  ;       "X"
+  ;       (game/take-next-turn game "O"))))
+
+  ; (it "prompts the next player for their next move"
+  ;     (let [game (game/new-game "X" "O")]
+  ;     (should=
+  ;       "O"
+  ;       (game/take-next-turn game "X")))))
 
 
 
--- src/ttt_clojure/game.clj
+++ src/ttt_clojure/game.clj
@@ -2,27 +2,76 @@
   (:require [ttt-clojure.board :as board]
             [ttt-clojure.cli :as cli]))
 
-(defn new-game [player1 player2]
-  {:player1 player1
-   :player2 player2})
 
-(defn- find-opposite-player [game current-player]
-    (if (= (:player1 game) current-player)
-      (:player2 game)
-      (:player1 game)))
 
-(defn take-next-turn [game current-player]
-  (find-opposite-player game current-player))
 
-(defn start [board current-player]
-  (let [move (cli/prompt-move board current-player)
-        new-board (board/make-move board
-                                   (:location move)
-                                   (:player move))]
-    (if (board/game-over? new-board)
-      (if (board/tie? new-board)
-        (cli/report-tie current-player)
-        (cli/report-winner current-player)))))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+; (defn new-game [player1 player2]
+;   {:player1 player1
+;    :player2 player2})
+
+; (defn- find-opposite-player [game current-player]
+;     (if (= (:player1 game) current-player)
+;       (:player2 game)
+;       (:player1 game)))
+
+; (defn take-next-turn [game current-player]
+;   (find-opposite-player game current-player))
+
+; (defn start [board current-player]
+;   (let [move (cli/prompt-move board current-player)
+;         new-board (board/make-move board
+;                                    (:location move)
+;                                    (:player move))]
+;     (if (board/game-over? new-board)
+;       (if (board/tie? new-board)
+;         (cli/report-tie current-player)
+;         (cli/report-winner current-player)))))
 
 
 
