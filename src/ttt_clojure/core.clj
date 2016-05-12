cc89dc78e2747ac569c5d3d44bc7e8f02d8ce978
Justin Holzmann <jholzmann@8thlight.com>
2016-05-12 16:33:14 -0500

    Adds in new-game logic to core so that it prompts a user if they want to play.

--- src/ttt_clojure/core.clj
+++ src/ttt_clojure/core.clj
@@ -1,6 +1,8 @@
 (ns ttt-clojure.core
-  (:require [ttt-clojure.game :as game])
+  (:require [ttt-clojure.game :as game]
+            [ttt-clojure.ui :as ui])
   (:gen-class))
 
 (defn -main []
-  (game/start))
+  (if (ui/new-game)
+    (game/start)))
