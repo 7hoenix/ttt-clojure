7f95c9ec409cba098f1a0919a256046f03cae5ec
Justin Holzmann <jholzmann@8thlight.com>
2016-05-17 11:54:59 -0500

    Take 3

--- spec/ttt_clojure/mini_max_spec.clj
+++ spec/ttt_clojure/mini_max_spec.clj
@@ -3,30 +3,57 @@
             [ttt-clojure.mini-max :as mm]
             [ttt-clojure.game :as game]))
 
-(describe "calculate"
-  (it "returns the next viable move"
-      (let [board ["X" "X" "O"
-                   "O" "X" "O"
-                   "X" "O" " "]
-            game (game/create-new-game board "X" "O")]
-      (should=
-               8
-               (mm/calculate game "X"))))
+(describe "minimax")
+  ; (it "returns the next viable move"
+  ;     (let [board ["X" "X" "O"
+  ;                  "O" "X" "O"
+  ;                  "X" "O" " "])
+  ;     (should=
+  ;              8
+  ;              (mm/minimax board "X" "O")))))
 
-  (it "returns a different viable move"
-      (let [board ["X" "X" " "
-                   "O" "X" "O"
-                   "X" "O" "O"]
-            game (game/create-new-game board "X" "O")]
-      (should=
-               2
-               (mm/calculate game "X"))))
+  ; (it "returns a different viable move"
+  ;     (let [board ["X" "X" " "
+  ;                  "O" "X" "O"
+  ;                  "X" "O" "O"]
+  ;           game (game/create-new-game board "X" "O")]
+  ;     (should=
+  ;              2
+  ;              (mm/minimax game "X"))))
 
-  (it "chooses the winning move if two are available"
-      (let [board ["O" "O" "X"
-                   "X" "X" " "
-                   " " "O" "O"]
-            game (game/create-new-game board "X" "O")]
-      (should=
-               6
-               (mm/calculate game "O")))))
+  ; (it "chooses the winning move if two are available"
+  ;     (let [board ["O" "O" "X"
+  ;                  "X" "X" " "
+  ;                  " " "O" "O"]
+  ;           invoking-player "O"
+  ;           other-player "X"]
+  ;     (should=
+  ;              6
+  ;              (mm/calculate board
+  ;                            invoking-player
+  ;                            other-player))))
+
+  ; (it "chooses a forcing move"
+  ;     (let [board [" " "X" " "
+  ;                  "O" "O" "X"
+  ;                  "X" "O" " "]]
+  ;     (should=
+  ;              2
+  ;              (mm/calculate board "X" "O")))))
+
+; (describe "score"
+;   (it "returns 10 for a win this turn"
+;     (let [board ["X" "X" "O"
+;                  "O" "X" "O"
+;                  "X" "O" "X"]]
+;           (should=
+;                    10
+;                    (mm/score board "X" "X"))))
+
+;   (it "returns -10 for your opponent winning this turn"
+;     (let [board ["X" "X" "O"
+;                  "O" "X" "O"
+;                  "X" "O" "O"]]
+;           (should=
+;                    -10
+;                    (mm/score board "X" "O")))))
--- src/ttt_clojure/mini_max.clj
+++ src/ttt_clojure/mini_max.clj
@@ -3,18 +3,25 @@
             [ttt-clojure.ttt-rules :as ttt]
             [ttt-clojure.board :as board]))
 
-(defn calculate [game player]
-  (let [board (:board game)
-        available (board/available-spaces board)
-        next-move (first available)]
-    (loop [board board
-           next-move next-move
-           available available
-           player player]
-      (let [new-board (board/make-move board next-move player)]
-        (if (ttt/game-is-over? new-board available)
-          next-move
-          (recur board
-                 (first available)
-                 (rest available)
-                 player))))))
+; (defn score [board invoking-player current-player]
+;   (cond
+;     (and (= invoking-player current-player)
+;          (= (ttt/winner board) current-player)) 10
+;     (and (not (= invoking-player current-player))
+;          (= (ttt/winner board) current-player)) -10
+;     :else 0))
+
+(defn minimax [])
+
+; board goes through each potential move and creates a new board representing that play
+; for each of these board states we go through and calculate each potential move
+; Keep going until we reach a terminating board (a board that ends in a win/loss/draw).
+; Each parent needs to choose the branch that represents the best choice for that player.
+; For the first level that player is choosing the maximum value returned.
+; For the second level that player is choosing the minimum value returned (since those values
+; are going to get passed as choices up to the other player).
+; For the third level that player is choosing the maximum value returned for each board.
+; Once we have reached terminating board positions those results need to be passed up the tree
+; to the parent...
+; The parent needs to look at the return values and save the very highest or lowest score to
+; go be passed up to its parent based on which players interest it is representing.
