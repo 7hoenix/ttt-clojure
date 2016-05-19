1dbce69b2d4e64e30e98f5909735768454afe7bd
Justin Holzmann <jholzmann@8thlight.com>
2016-05-19 15:10:48 -0500

    Maybe its working?

--- spec/ttt_clojure/mini_max_spec.clj
+++ spec/ttt_clojure/mini_max_spec.clj
@@ -8,5 +8,13 @@
                    "O" "O" "X"
                    "X" "O" " "]]
         (should=
-                 2
-                 (mm/minimax board "X" "O")))))
+                2
+                (mm/minimax board "X" "O"))))
+
+  (it "correctly figures out a different move"
+      (let [board [" " "X" " "
+                   "O" "O" "X"
+                   "X" "O" "O"]]
+        (should=
+                0
+                (mm/minimax board "O" "X")))))
--- src/ttt_clojure/mini_max.clj
+++ src/ttt_clojure/mini_max.clj
@@ -2,61 +2,52 @@
   (:require [ttt-clojure.board :as board]
             [ttt-clojure.ttt-rules :as ttt]))
 
-(declare minimax maxi mini)
+(declare minimax maxi)
 
 (defn- game-over? [board]
   (ttt/game-is-over? board
                      (board/available-spaces board)))
 
-(defn score [board multiplier]
+(defn score [board current-player]
   (if (ttt/winner board)
-    (if (> 0 multiplier)
+    (if (= ttt/winner current-player)
       10
       -10)
     0))
 
-(defn which-player [player1 player2 multiplier]
-  (if (> 0 multiplier)
-    player2
-    player1))
+(defn best-score [tree current-player]
+  ; (println (str "basic tree" tree " current-player " current-player))
+  (reduce (fn [itm comparison]
+      ; (println (str "itm " itm " comparison " comparison))
+            (if (empty? itm)
+              comparison
+              (if (< (second comparison) (second itm))
+                itm
+                comparison)))
+          tree))
 
-(defn best-score [tree multiplier]
-  (println (str "best score: " tree))
-  (println (str "max score " (apply max tree)))
-  (println (str "min score: " (apply min tree)))
-  (if (< 0 multiplier)
-    (apply max tree)
-    (apply min tree)))
+(defn- mm-algorithm [board current-player opponent-player location multiplier]
+   (if (game-over? board)
+     (vector location (* multiplier (score board current-player)))
+     (let [available (board/available-spaces board)
+           states (map (fn [spot]
+                         (vector spot
+                                (board/make-move board
+                                                 spot
+                                                 current-player)))
+                       available)
+           tree (reduce (fn [bb [location board]]
+                          (conj bb (maxi board
+                                         current-player
+                                         opponent-player
+                                         location
+                                         multiplier)))
+                        []
+                        states)]
+       (best-score tree current-player))))
 
-(defn minimax ([board player1 player2] (minimax board maxi player1 player2 1))
-  ([board func player1 player2 multiplier]
-    (if (game-over? board)
-      (or (println (str "score: " (score board multiplier) " multiplier: " multiplier))
-          (score board multiplier))
-      (let [available (board/available-spaces board)
-            player (which-player player1
-                                 player2
-                                 multiplier)
-            states (map #(board/make-move board
-                                          %
-                                          player)
-                                          available)
-            tree (reduce (fn [bb board]
-                           (println (str "bb: " bb))
-                   (conj bb
-                         (func bb
-                               board
-                               player1
-                               player2
-                               multiplier)))
-                []
-                states)]
-        (best-score tree multiplier)))))
+(defn minimax [board current-player opponent-player]
+  (first (mm-algorithm board current-player opponent-player nil 1)))
 
-(defn maxi [best-board board player1 player2 multiplier]
-  (println (str "maxi: " multiplier))
-  (minimax board mini player1 player2 (* -2 multiplier)))
-
-(defn mini [best-board board player1 player2 multiplier]
-  (println (str "mini: " multiplier))
-  (minimax board maxi player1 player2 (* -3 multiplier)))
+(defn maxi [board current-player opponent-player location multiplier]
+  (mm-algorithm board opponent-player current-player location (* -1 multiplier)))
