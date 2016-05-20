a97195725cafee462ea7c68c71711d47210b6cca
Justin Holzmann <jholzmann@8thlight.com>
2016-05-20 09:33:27 -0500

    Adds to remove print screens from test suite.

--- spec/ttt_clojure/ai_spec.clj
+++ spec/ttt_clojure/ai_spec.clj
@@ -5,6 +5,9 @@
             [ttt-clojure.game :as game]))
 
 (describe "ai"
+  (around [it]
+          (with-out-str (it)))
+
   (it "can play against itself"
     (let [game (game/create-new-game "X" "O")
           end-board (game/start game "X" "O" println ai/minimax)]
