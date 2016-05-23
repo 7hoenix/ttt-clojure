c01e3197e1d64d8a8e4eda86493fd0dcb3e0a9de
Justin Holzmann <jholzmann@8thlight.com>
2016-05-23 16:28:03 -0500

    Adds in defrecord for AI which calls the player protocol.

--- src/ttt_clojure/ai.clj
+++ src/ttt_clojure/ai.clj
@@ -1,8 +1,17 @@
 (ns ttt-clojure.ai
-  (:require [ttt-clojure.mini-max :as mm]))
+  (:require [ttt-clojure.mini-max :as mm]
+            [ttt-clojure.player :as player]))
 
-(defn minimax [board current-player opponent-player printer]
+(defn minimax [board current-player opponent-player]
   {:location (mm/minimax board
                          current-player
                          opponent-player)
    :player current-player})
+
+(defrecord AI [mark opponent-mark]
+  player/PPlayer
+  (display [this _] nil)
+  (take-turn [this board] (minimax board mark opponent-mark)))
+
+(defn create-ai [mark opponent-mark]
+  (->AI mark opponent-mark))
