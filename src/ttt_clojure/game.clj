1ac935e5aced916e6e8b2421d5a7ff0140b757c2
Justin Holzmann <jholzmann@8thlight.com>
2016-05-23 16:29:33 -0500

    Forces a user to pass in two players (implying something to get settings from a user)

--- src/ttt_clojure/game.clj
+++ src/ttt_clojure/game.clj
@@ -1,18 +1,14 @@
 (ns ttt-clojure.game
   (:require [ttt-clojure.board :as board]
-            [ttt-clojure.ui :as ui]
+            [ttt-clojure.player :as player]
             [ttt-clojure.ttt-rules :as ttt]))
 
-(defn create-new-game ([] (create-new-game "X" "O"))
-  ([player1 player2]
-   (create-new-game
-     (board/new-board)
-     player1
-     player2))
-  ([board player1 player2]
-   {:board board
-    :player1 player1
-    :player2 player2}))
+(defn create-new-game ([player1 player2] (create-new-game (board/new-board)
+                                                          player1
+                                                          player2))
+  ([board player1 player2] {:board board
+                            :player1 player1
+                            :player2 player2}))
 
 (defn- game-over? [game]
   (let [board (:board game)]
@@ -20,18 +16,7 @@
                        (board/available-spaces board))))
 
 (defn- game-has-winner? [game]
-    (not (ttt/is-tie? (board/available-spaces (:board game)))))
-
-(defn get-other-player [game current-player]
-  (if (= (:player1 game) current-player)
-    (:player2 game)
-    (:player1 game)))
-
-(defn get-next-move [board current-player opponent-player disp-func prompt-func]
-  (prompt-func board
-               current-player
-               opponent-player
-               disp-func))
+  (not (ttt/is-tie? (board/available-spaces (:board game)))))
 
 (defn make-move [game move]
   (let [new-board (board/make-move (:board game)
@@ -40,22 +25,14 @@
         mutated-game (conj game {:board new-board})]
     mutated-game))
 
-(defn start
-  ([] (start (create-new-game)))
-  ([game] (start game (:player1 game) (:player2 game) println read-line))
-  ([game current-player opponent-player disp-func prompt-func]
-   (ui/print-board disp-func (:board game))
+(defn start ([game] (start game (:player1 game) (:player2 game) println read-line))
+  ([game current-player opponent print-func prompt-func]
    (if (game-over? game)
-     (if (game-has-winner? game)
-       (and (ui/report-winner disp-func
-                         opponent-player)
-            (:board game))
-       (and (ui/report-tie disp-func "tie")
-            (:board game)))
+     (or (if (game-has-winner? game)
+           (player/display current-player (str opponent " wins"))
+           (player/display current-player (str "Cats game")))
+         (:board game))
      (let [mutated-game (make-move game
-                                   (get-next-move (:board game)
-                                                  current-player
-                                                  opponent-player
-                                                  disp-func
-                                                  prompt-func))]
-       (recur mutated-game opponent-player current-player disp-func prompt-func)))))
+                                   (player/take-turn current-player
+                                                     (:board game)))]
+       (recur mutated-game opponent current-player print-func prompt-func)))))
