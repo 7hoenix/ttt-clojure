ce4e8cb4ab2bb99f002f66501945aa0221222251
Justin Holzmann <jholzmann@8thlight.com>
2016-05-26 13:47:56 -0500

    Fresh slate.

--- spec/ttt_clojure/mini_max_spec.clj
+++ spec/ttt_clojure/mini_max_spec.clj
@@ -9,28 +9,28 @@
                    "X" "O" " "]]
         (should=
                 2
-                (mm/minimax board "O" "X"))))
-
-  (it "correctly figures out a different move"
-      (let [board [" " "X" " "
-                   "O" "O" "X"
-                   "X" "O" "O"]]
-        (should=
-                0
-                (mm/minimax board "O" "X"))))
-
-  (it "basic"
-      (let [board ["O" " " "X"
-                   "X" " " " "
-                   "X" "O" "O"]]
-        (should=
-                4
-                (mm/minimax board "X" "O"))))
-
-  (it "depth needed"
-      (let [board [" " "X" " "
-                   " " " " "X"
-                   "O" "O" "X"]]
-        (should=
-                2
                 (mm/minimax board "O" "X")))))
+
+  ; (it "correctly figures out a different move"
+  ;     (let [board [" " "X" " "
+  ;                  "O" "O" "X"
+  ;                  "X" "O" "O"]]
+  ;       (should=
+  ;               0
+  ;               (mm/minimax board "O" "X"))))
+
+  ; (it "basic"
+  ;     (let [board ["O" " " "X"
+  ;                  "X" " " " "
+  ;                  "X" "O" "O"]]
+  ;       (should=
+  ;               4
+  ;               (mm/minimax board "X" "O"))))
+
+  ; (it "depth needed"
+  ;     (let [board [" " "X" " "
+  ;                  " " " " "X"
+  ;                  "O" "O" "X"]]
+  ;       (should=
+  ;               2
+  ;               (mm/minimax board "O" "X")))))
