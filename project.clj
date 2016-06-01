0f01ddfe62c1c6a579706a8c8dd21a49555e39c0
Justin Holzmann <jholzmann@8thlight.com>
2016-06-01 16:04:37 -0500

    Changes clojure version to 1.8 to get access to includes method. Also puts in ring.

--- project.clj
+++ project.clj
@@ -3,8 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-RC2"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [ring/ring-mock "0.3.0"]]
   :profiles {:dev {:dependencies [[speclj "3.3.1"]]}}
-  :plugins [[speclj "3.3.1"]]
+  :plugins [[speclj "3.3.1"]
+            [ring "1.5.0-RC1"]]
   :test-paths ["spec"]
   :main ttt-clojure.core)
