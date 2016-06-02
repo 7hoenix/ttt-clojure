35f885893208be4e73ad8d3d9187d1feb60fdbc3
Justin Holzmann <jholzmann@8thlight.com>
2016-06-02 13:25:22 -0500

    Moves ring from plugins into dependencies... not sure the difference

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [ring/ring-mock "0.3.0"]]
+                 [ring/ring-mock "0.3.0"]
+                 [ring "1.5.0-RC1"]]
   :profiles {:dev {:dependencies [[speclj "3.3.1"]]}}
-  :plugins [[speclj "3.3.1"]
-            [ring "1.5.0-RC1"]]
+  :plugins [[speclj "3.3.1"]]
   :test-paths ["spec"]
   :main ttt-clojure.core)
