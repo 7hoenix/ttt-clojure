daa6f9d83c3810daf88bdb5495727354317a72c2
Justin Holzmann <jholzmann@8thlight.com>
2016-06-02 13:25:49 -0500

    Adds reset-games function to clear atoms between tests. Also tests making a move.

--- spec/ttt_clojure/server_spec.clj
+++ spec/ttt_clojure/server_spec.clj
@@ -7,8 +7,12 @@
             [clojure.string :as string]
             [ring.mock.request :as mock]))
 
+(defn reset-games []
+  (reset! server/games {}))
+
 (describe "GET /new-game"
           (it "displays a message to start a new game"
+              (reset-games)
               (should=
                 true
                 (string/includes?
@@ -17,6 +21,7 @@
 
 (describe "POST /games"
           (it "returns a newly created game"
+              (reset-games)
               (let [game1 (server/app (mock/request :post "/games"))]
                 (should=
                   [" " " " " " " " " " " " " " " " " "]
@@ -26,9 +31,21 @@
 
 (describe "GET /games/1"
           (it "should display the game with id of 1"
+              (reset-games)
               (let [game (server/app (mock/request :post "/games"))]
                 (should=
                         [" " " " " " " " " " " " " " " " " "]
+                        (:board (get
+                                  (:body (server/app (mock/request :get "/games/1")))
+                                  1))))))
+
+(describe "PUT /games/1?symbol=X&spot=0"
+          (it "should return a board for game 1 with a move shown"
+              (reset-games)
+              (let [game (server/app (mock/request :post "/games"))]
+                (server/app (mock/request :put "/games/1?symbol=X&spot=0"))
+                (should=
+                        ["X" " " " " " " " " " " " " " " " "]
                         (:board (get
                                   (:body (server/app (mock/request :get "/games/1")))
                                   1))))))
